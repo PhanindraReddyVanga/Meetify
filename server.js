c47c90466a189fdf2a9a0d7d53235d9f6b9d9385
PhanindraReddyVanga <phanindrareddy1507@gmail.com>
2026-01-21 14:29:19 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app");
-require("dotenv").config();
-
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-  console.log(`Meetify backend running on port ${PORT}`);
-});
